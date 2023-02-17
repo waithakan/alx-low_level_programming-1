8d112420349497075b33f9ce5dbf79701cf745d1
coollins awino <collinsawino678@gmail.com>
2023-02-17 07:14:22 -0800

    ALPHABETS.C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the main function
+ * Return:if 0 exit properly, non-zero otherrwise
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
