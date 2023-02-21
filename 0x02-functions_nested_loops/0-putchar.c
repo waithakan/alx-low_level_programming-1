58923a3a57237b34a738f146a465faf5966bef01
coollins awino <collinsawino678@gmail.com>
2023-02-21 08:00:39 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-prints _putchar and is the main function of the programm
+ * Return:if zero exit correctly if non-zero, otherwise
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
