720ab64a1cd171c18c35d502595904af66e2c8a6
coollins awino <collinsawino678@gmail.com>
2023-02-23 01:10:51 -0800

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <stdio.h>
 int largest_numer(int a, int b, int c);
 int main(void);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-
-#endif 
+#endif
