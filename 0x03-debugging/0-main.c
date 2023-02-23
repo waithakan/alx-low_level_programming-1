fd3e8f4061478df9932327cfe32b1a62a6cfaa1c
coollins awino <collinsawino678@gmail.com>
2023-02-23 02:05:53 -0800

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
+
 #include "main.h"
 /**
  * main-test if the function is poositive or negative
- * Return: if 0 exit properly, non-zero otherwise
  */
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive _or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,23 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)
+ * positive_or_negative - checks for positive or negative integers
+ * Return:if 0 exit properly
+ * @i: the integer ti be checked
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	if (i > 0)
+		printf("%d the number is positive\n", i);
+	else if (i == 0)
+		printf("%d the number is zero\n", i);
+	else
+		printf("%d the number is negative\n", i);
 	return (0);
 }
