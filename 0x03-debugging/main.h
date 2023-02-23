fab895bba79b55ae778a50e14b8960c3b69181d6
coollins awino <collinsawino678@gmail.com>
2023-02-23 02:11:27 -0800

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,6 @@ int largest_numer(int a, int b, int c);
 int main(void);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+void positive_or_negative(int i);
 
 #endif
