e69b362c2468d3ed2eac4decac267d599d0bcbda
coollins awino <collinsawino678@gmail.com>
2023-02-23 00:25:23 -0800

    main

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints positive or negative integer
+ * Return: if 0 exit properly
+ */
+
+int main(void)
+{
+	int i;
+	
+
+	i = 98;
+	
+	positive_or_negative(i);
+	
+	return (0);
+}
