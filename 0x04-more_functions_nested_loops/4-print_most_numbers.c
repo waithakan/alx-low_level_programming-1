04096cc8bb45c8f24135115d0e827e53b6880c61
coollins awino <collinsawino678@gmail.com>
2023-02-23 17:47:30 -0800

    except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**print_most_numbers - prints 0 to 9 except 2 and 4
+ * followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; 10 > i; i++)
+	{
+		if (i != 2 && i !=4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
