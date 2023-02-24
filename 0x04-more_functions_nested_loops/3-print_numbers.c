47f7553bd2fe87a77833aa5658d0953afba819e4
coollins awino <collinsawino678@gmail.com>
2023-02-23 17:28:35 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints digits from 0 to 9
+ * followed by a new line
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		_putchar(j + '0');
+	}
+	_putchar('\n');
+}
