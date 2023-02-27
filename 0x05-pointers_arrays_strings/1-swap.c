6997716e176483184aa5e9d90f1377207f8b0edb
coollins awino <collinsawino678@gmail.com>
2023-02-27 06:37:32 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - swaps value of integers
+ * @a: points to first value
+ * @b: points to second valuea to be swaped
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+
+	*a = *b;
+
+	*b = c;
+
+	return (0);
+}
