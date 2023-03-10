0b689620b8b1eb852af9ddb3538467d8880e1a41
coollins awino <collinsawino678@gmail.com>
2023-03-10 07:39:02 -0800

    #HARD THINGS

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,18 +4,10 @@
 /**
  * main - prints all arguments it receives
  * @argc: number of arguments
- * @argv: array of arguments
- *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc)
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-
+	printf("argc = %d\n",argc);
 	return (0);
 }
