65cc0d0195f5cf81bb62b52c932582a3a9750880
coollins awino <collinsawino678@gmail.com>
2023-03-10 07:40:56 -0800

    #HARD THINGS

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,14 @@
 #include "main.h"
 
 /**
- * main - prints all arguments it receives
+ * main - print the number of arguments passed to the program
  * @argc: number of arguments
+ * @argv: array of arguments
+ *
  * Return: Always 0 (Success)
  */
-int main(int argc)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("argc = %d\n",argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
