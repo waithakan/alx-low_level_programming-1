ad1da88f53ded7e35ef5e073b39f0a7c0a4bc588
coollins awino <collinsawino678@gmail.com>
2023-04-03 23:20:03 -0700

    function to delete a node at an index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 	current = temp->next;
-	temp->next = current->nextt;
+	temp->next = current->next;
 
 	return (1);
 }
