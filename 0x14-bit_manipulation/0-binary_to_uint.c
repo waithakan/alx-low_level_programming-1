327370d74244eec3c1ccce9139028f7b1b5fcd85
coollins awino <collinsawino678@gmail.com>
2023-04-08 01:51:08 -0700

    function that converts binary number into unsigned integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_unit(const char *b)
 	{
 		if (b[j] < '0' || b[j] > '1')
 			return (0);
-		dec_val = 2 * val + (b[j] - '0');
+		dec_val = 2 * dec_val + (b[j] - '0');
 	}
 	return (dec_val);
 }
